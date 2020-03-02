2226bdcd7d8eecf88030888e4a8f30ea10aae045
Simi Falaye <Simi.Falaye@vecima.com>
2020-03-02 13:03:25 -0600

    mastermonitor & tilegap

--- config.def.h
+++ config.def.h
@@ -1,7 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 
+/* master monitor */
+static const unsigned int MASTERMON  = 1;        /* master monitor [1-...] */
+
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 6;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
